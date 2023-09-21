2404cd5c37322976dc35fb1963883653fc33423a
WillingtonM <willington.mhlanga@gmail.com>
2023-09-21 06:50:09 +0200

    Will's commit

--- models/state.py
+++ models/state.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-""" 
-    State Module for HBNB project 
-"""
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
@@ -14,7 +12,6 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == "db":
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="delete, delete-orphan")
+        cities = relationship("City", backref="state")
     else:
         name = ""

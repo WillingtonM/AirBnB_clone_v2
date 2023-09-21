c58f9b0cfb5072ad64c078a1db84245c70125f62
WillingtonM <willington.mhlanga@gmail.com>
2023-09-21 08:19:40 +0200

    Will's commit

--- models/place.py
+++ models/place.py
@@ -2,23 +2,21 @@
 """
     Place module for the HBNB project
 """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 import models
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from os import environ
-from models.review import Review, Amenity
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy import Column, Float, Integer, ForeignKey, String, Table
+from sqlalchemy.orm import relationship
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
-if storage_engine == "db":
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
+association_table = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60), ForeignKey("places.id"),
                                  primary_key=True, nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
                                  primary_key=True, nullable=False))
 
 
@@ -49,15 +47,15 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
+    number_rooms = Column(Integer, default=0)
+    number_bathrooms = Column(Integer, default=0)
+    max_guest = Column(Integer, default=0)
+    price_by_night = Column(Integer, default=0)
+    latitude = Column(Float)
+    longitude = Column(Float)
     reviews = relationship("Review", backref="place", cascade="delete")
     amenities = relationship("Amenity", secondary="place_amenity",
-                            viewonly=False)
+                             viewonly=False)
     
     amenity_ids = []
     
